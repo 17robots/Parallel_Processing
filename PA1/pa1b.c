18816da5b6fd64caa719b58e2d760574a652192f
17robots <mdray@ameritech.net>
2020-09-26 04:32:53 -0400

    Added Part B

--- PA1/pa1b.c
+++ PA1/pa1b.c
@@ -1,48 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <string.h>
-#include <sys/types.h> /* typedefs */
-#include <sys/stat.h>
 
-// void mat_vect_mult(int local_A[], int local_x[], int local_y[], int local_rows, int cols, int local_n, MPI_Comm comm)
-// {
-//   double *x;
-//   int local_i, j;
-//   int local_ok = 1;
-//   x = malloc(cols * sizeof(double));
-//   MPI_Allgather(local_x, local_n, MPI_INT, x, local_n, MPI_INT, comm);
-
-//   for (local_i = 0; local_i < local_rows; ++local_i)
-//   {
-//     local_y[local_i] = 0.0;
-//     for (j = 0; j < cols; ++j)
-//     {
-//       local_y[local_i] += local_A[local_i * cols + j] * x[j];
-//     }
-//   }
-//   free(x);
-// }
-
-void mat_vect_mult(int local_A[], int n[], int local_y[], int local_rows, int cols, MPI_Comm comm)
+void mat_mult(int *local_a, int *local_y, int *x, int loc_rows, int cols)
 {
-  for (int i = 0; i < local_rows; ++i)
+  for (int i = 0; i < loc_rows; ++i)
   {
     local_y[i] = 0;
     for (int j = 0; j < cols; ++j)
     {
-      local_y[i] += local_A[i * local_rows + j] * n[j];
+      local_y[i] += local_a[i * cols + j] * x[j];
     }
   }
-  free(x);
 }
 
-int main(void)
+int main()
 {
+  int size, rank;
   int rows, cols;
-  int *a, *n, *y, *local_a, *local_x, *local_y;
-  int rank, size;
-
+  int *y, *local_y, *a, *local_a, *x;
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -51,41 +27,49 @@ int main(void)
   {
     FILE *file;
     file = fopen("mv-data.txt", "r");
-
-    if (!file)
-    {
-      printf("Cannot open file\n");
-      return 1;
-    }
-
     fscanf(file, "%d", &rows);
     fscanf(file, "%d", &cols);
 
     a = malloc(sizeof(int) * rows * cols);
-    n = malloc(cols * sizeof(int));
-    y = malloc(sizeof(int) * cols);
-
     for (int i = 0; i < rows * cols; ++i)
-    {
       fscanf(file, "%d", &a[i]);
-    }
 
+    x = malloc(sizeof(int) * cols);
     for (int i = 0; i < cols; ++i)
+      fscanf(file, "%d", &x[i]);
+
+    printf("Process %d allocating memory\n", rank);
+    for (int i = 0; i < size; ++i)
     {
-      fscanf(file, "%d", &n[i]);
+      MPI_Send(&rows, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+      MPI_Send(&cols, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+      MPI_Send(x, cols, MPI_INT, i, i, MPI_COMM_WORLD);
     }
-    MPI_Scatter(a, cols, MPI_INT, local_a, cols, MPI_INT, 0, comm);
   }
 
-  mat_vect_mult(local_a, n, local_y, rows / size, cols, MPI_WORLD_COMM);
+  if (rank != 0)
+  {
+    MPI_Recv(&rows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&cols, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    x = malloc(sizeof(int) * cols);
+    MPI_Recv(x, cols, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+
+  local_a = malloc(sizeof(int) * cols);
+  local_y = malloc(sizeof(int) * rows / size);
+  MPI_Scatter(a, cols, MPI_INT, local_a, cols, MPI_INT, 0, MPI_COMM_WORLD);
+
+  mat_mult(local_a, local_y, x, rows / size, cols);
+
+  y = malloc(sizeof(int) * rows);
+  MPI_Gather(local_y, rows / cols, MPI_INT, y, rows / cols, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (rank == 0)
   {
-    MPI_Gather(local_y, size, MPI_INT, y, size, MPI_INT, MPI_WORLD_COMM);
+    printf("[ ");
     for (int i = 0; i < size; ++i)
-    {
       printf("%d ", y[i]);
-    }
+    printf("]\n");
   }
 
   MPI_Finalize();
