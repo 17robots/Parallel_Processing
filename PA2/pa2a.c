a1f032a397af172a94f51afcd7c6433f1fa16926
17robots <mdray@ameritech.net>
2020-10-21 14:25:13 -0400

    Adding PartA

--- PA2/pa2a.c
+++ PA2/pa2a.c
@@ -1,6 +1,32 @@
 #include <pthread.h>
 
+// we need to go for each number of threads 1, 2, 4, 8 and then we need to go from threads 1000, 2000, 4000, 8000, 16000
+
+void *calculateFactorial(int numElems, int startIndex, int *numArr)
+{
+}
+
 int main()
 {
+  int for (int threadCount = 1; threadCount < 8; threadCount *= 2)
+  {
+    // set up the threads
+    pthread_t threads[threadCount];
+    pthread_attr_t attr;
+
+    for (int arrLength = 1000; arrLength < 16001; arrLength *= 2)
+    {
+    }
+
+    // clear attributes
+    pthread_attr_destroy(&attr);
+
+    // kill all threads
+    for (int i = 0; i < threadCount; ++i)
+    {
+      pthread_join(&threads[i], NULL);
+    }
+  }
+  pthread_exit(NULL);
   return 0;
 }
